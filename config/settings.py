76cc65b5f5741bd4e919017d1f838c6618cbd90c
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-06-15 16:35:52 +0500

    fixed

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*722buw0n9rnp0#*)xvsq8y31d^rvqeg88t$-yim%momzp8x5i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
